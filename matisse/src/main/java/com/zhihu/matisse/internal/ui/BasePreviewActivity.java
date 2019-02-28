412e7e54e442351461b32a0e231f7cf0c4010b2e
panpanini <panpanini@mercari.com>
2019-02-28 16:11:23 +0900

    🔧 Fix androidX imports

--- matisse/src/main/java/com/zhihu/matisse/internal/ui/BasePreviewActivity.java
+++ matisse/src/main/java/com/zhihu/matisse/internal/ui/BasePreviewActivity.java
@@ -19,9 +19,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
